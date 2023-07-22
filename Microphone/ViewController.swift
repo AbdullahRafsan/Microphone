04ac5c725dfa5342a1a1d0511fcf7e1dfc4f03bc
Abdullah Rafsan <rafsanabdullah88@gmail.com>
2023-07-22 08:02:55 +0600

    Some house keeping.

--- Microphone/ViewController.swift
+++ Microphone/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import AVFoundation
 
 
-class ViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
+class ViewController: UIViewController {
     
     @IBOutlet weak var microphoneToggleButton: UIButton!
     var audiosession:AVAudioSession!
@@ -19,23 +19,59 @@ class ViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDe
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        preparetool()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        preparetool()
+        start()
+    }
+    
+    @objc func handleRouteChange(notification:Notification){
+        
+        guard let userInfo = notification.userInfo,
+              let reasonKey = userInfo[AVAudioSessionRouteChangeReasonKey] as? UInt,
+              let reason = AVAudioSession.RouteChangeReason(rawValue: reasonKey) else {
+            return
+        }
+        switch reason {
+        case .newDeviceAvailable:
+            if !audiosession.currentRoute.outputs.filter({ $0.portName.lowercased().contains("idam") }).isEmpty {
+                performWork()
+            }
+        case .oldDeviceUnavailable:
+            let _ = audiosession.currentRoute.outputs.filter(){
+                pd in
+                if !pd.portName.lowercased().contains("idam") {
+                    haltWork()
+                    alert("আইফোনকে ম্যাক থেকে বিচ্ছিন্ন করা হয়েছে ।")
+                }
+                
+                return true
+            }
+        default: ()
+        }
+    }
+    
+    
     // Prepare the application
+    /*
+     Try to prepare the AVAudioSession, ask for recording permission
+     and set appropriate recording state
+     */
     func preparetool(){
         do{
+            
             audiosession = AVAudioSession.sharedInstance()
             audioengine = AVAudioEngine()
             try audiosession.setCategory(.playAndRecord, mode: .videoRecording, options: [.duckOthers ,.defaultToSpeaker])
             try audiosession.setActive(true)
+            NotificationCenter.default.addObserver(self, selector: #selector(handleRouteChange), name: AVAudioSession.routeChangeNotification, object: nil)
             switch audiosession.recordPermission {
             case AVAudioSession.RecordPermission.granted:
                 isGranted = true
                 break
             case AVAudioSession.RecordPermission.denied:
                 isGranted = false
-                alert("রেকর্ড করবার অনুমতি পাওয়া যায়নি !")
                 break
             case AVAudioSession.RecordPermission.undetermined:
                 audiosession.requestRecordPermission(){
@@ -56,23 +92,21 @@ class ViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDe
         }
         
         catch {
-            isWorking = false
-            changeButtonAppearance()
             alert("দুঃখিত, কিছু একটা সমস্যা হয়েছে । দয়া করে আবার চেষ্টা করুন ।")
         }
     }
     
-    
+    // Button action
     @IBAction func start(){
-        if isWorking {
-            haltWork()
-        }
-        else {
-            performWork()
-        }
-        
+            if isWorking {
+                haltWork()
+            }
+            else {
+                performWork()
+            }
     }
     
+    // Change the button style depending on the state
     func changeButtonAppearance(){
         if isWorking {
             microphoneToggleButton.backgroundColor = UIColor.systemRed
@@ -84,35 +118,45 @@ class ViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDe
         }
     }
     
+    // Start operation
+    /*
+     If we are permitted then check we are connected to the IDAM output,
+     otherwise do nothing or show appropriate error
+     
+     */
     func performWork(){
         if isGranted {
-            isWorking = true
-            changeButtonAppearance()
-            do{
-                audioengine.connect(self.audioengine.inputNode, to: self.audioengine.mainMixerNode, format: self.audioengine.inputNode.outputFormat(forBus: 0))
-                try audioengine.start()
+            if audiosession.currentRoute.outputs[0].portName.lowercased().contains("idam") {
+                do{
+                    audioengine.connect(self.audioengine.inputNode, to: self.audioengine.mainMixerNode, format: self.audioengine.inputNode.outputFormat(forBus: 0))
+                    try audioengine.start()
+                    isWorking = true
+                }
+                catch{
+                    isWorking = false
+                    alert("দুঃখিত, সার্ভিসটি চালু করা যায়নি ।")
+                }
             }
-            catch{
+            else{
                 isWorking = false
-                changeButtonAppearance()
-                alert("দুঃখিত, কিছু একটা সমস্যা হয়েছে । দয়া করে আবার চেষ্টা করুন ।")
+                alert("Audio MIDI Setup অ্যাপ থেকে আপনার আইফোনটি Enable করুন")
             }
         }
         else{
             isWorking = false
-            changeButtonAppearance()
             alert("রেকর্ড করবার অনুমতি পাওয়া যায়নি !")
         }
-        
+        changeButtonAppearance()
     }
     
+    // Stop operation
     func haltWork(){
         audioengine.stop()
         isWorking = false
         changeButtonAppearance()
     }
     
-    // Show alerts
+    // Create and show alerts
     func alert(_ msg:String){
         let alertcontroller = UIAlertController(title: nil, message: msg, preferredStyle: .alert)
         let ok = UIAlertAction(title: "ঠিক আছে", style: .cancel)
